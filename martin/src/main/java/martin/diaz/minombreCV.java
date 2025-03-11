f2bf44e17b19dcc76a220db17c559a7088a6969c
martdbj <martdbj@gmail.com>
2025-03-11 17:14:03 +0100

    update

---
+++ martin/src/main/java/martin/diaz/minombreCV.java
@@ -0,0 +1,13 @@
+package martin.diaz;
+
+/**
+ * Hello world!
+ *
+ */
+public class minombreCV
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
