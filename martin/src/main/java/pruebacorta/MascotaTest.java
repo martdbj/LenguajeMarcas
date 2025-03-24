3a200a346cc5466ff242c6e6a68637aa3aca3646
Martín Díaz Bastida <martdbj@gmail.com>
2025-03-24 18:08:10 +0100

    update

---
+++ martin/src/main/java/pruebacorta/MascotaTest.java
@@ -0,0 +1,38 @@
+package pruebacorta;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+public class MascotaTest {
+    final static String RUTA_FICH = "/home/fluza/Desktop/Daw_github/Lenguaje_marcas/martin/src/main/java/pruebacorta/";
+    static Mascotas mascotasClase = new Mascotas();
+    public static void main(String[] args) {
+        unmarshallingMascotas();
+        marshallingMascotas();
+    }
+    private static void unmarshallingMascotas() {
+        try {
+            JAXBContext contexto = JAXBContext.newInstance(Mascotas.class);
+            Unmarshaller um = contexto.createUnmarshaller();
+            mascotasClase = (Mascotas) um.unmarshal(new File(RUTA_FICH+"mascotas.xml"));
+            System.out.println("Lista de mascotas leidas: " + mascotasClase);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void marshallingMascotas() {
+        try {
+            JAXBContext contexto = JAXBContext.newInstance(Mascotas.class);
+            Marshaller  marshaller = contexto.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(mascotasClase, new File(RUTA_FICH+"mascotasOUT.xml"));
+            System.out.println("XML de salida generado correctamente");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
